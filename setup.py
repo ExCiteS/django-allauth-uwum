37123ff05bd26c22dcad5cdf39ffd338fff233bd
Julius Osokinas <josokinas@users.noreply.github.com>
2016-10-07 10:59:08 +0100

    Package without tests

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     author='Julius Osokinas',
     author_email='j.osokinas@gmail.com',
     license='MIT',
-    packages=find_packages(exclude=['*.tests', '*.tests.*', 'tests.*']),
+    packages=find_packages(),
     include_package_data=True,
     install_requires=['django-allauth >= 0.27.0'],
     classifiers=[
