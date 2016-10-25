80991add4a5fb7324e5326086a5988689a59e033
Julius Osokinas <josokinas@users.noreply.github.com>
2016-10-25 14:48:05 +0100

    Refresh the access token

--- allauth_uwum/client.py
+++ allauth_uwum/client.py
@@ -16,6 +16,8 @@ class UWUMClient(OAuth2Client):
 
     def get_access_token(self, code):
         """Get the access token."""
+        access_token = None
+
         data = {
             'grant_type': 'authorization_code',
             'client_id': self.consumer_key,
@@ -23,20 +25,7 @@ class UWUMClient(OAuth2Client):
             'code': code,
         }
 
-        cert = UWUMProvider.settings.get('CERT')
-
-        if not path.exists(cert):
-            raise OAuth2Error('UWUM certificate not found')
-
-        response = request(
-            self.access_token_method,
-            self.access_token_url,
-            headers=self.headers,
-            cert=cert,
-            data=data,
-        )
-
-        access_token = None
+        response = self._make_request(data)
 
         if response.status_code == 200:
             access_token = response.json()
@@ -46,3 +35,47 @@ class UWUMClient(OAuth2Client):
             raise OAuth2Error(error)
 
         return access_token
+
+    def refresh_access_token(self, refresh_token):
+        """Refresh the access token."""
+        access_token = None
+
+        data = {
+            'grant_type': 'refresh_token',
+            'refresh_token': refresh_token,
+            'scope': self.scope,
+        }
+
+        response = self._make_request(data)
+
+        if response.status_code == 200:
+            access_token = response.json()
+
+        if not access_token or 'access_token' not in access_token:
+            error = 'Error refreshing access token: %s' % response.content
+            raise OAuth2Error(error)
+
+        return access_token
+
+    def _get_cert(self):
+        """Get the UWUM certificate."""
+        cert = UWUMProvider.settings.get('CERT')
+
+        if not path.exists(cert):
+            raise OAuth2Error('UWUM certificate not found')
+
+        return cert
+
+    def _make_request(self, data):
+        """Make the request to UWUM."""
+        cert = self._get_cert()
+
+        response = request(
+            self.access_token_method,
+            self.access_token_url,
+            headers=self.headers,
+            cert=cert,
+            data=data,
+        )
+
+        return response
